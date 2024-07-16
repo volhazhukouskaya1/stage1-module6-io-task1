9c7fed2358f4fa1bdb7b062a78096a7f20869625
volhazhukouskaya1 <volha_zhukouskaya@outlook.com>
2024-07-16 10:38:51 +0300

    Module 6 - task 1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,44 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String name = null;
+        int age = 0;
+        String email = null;
+        Long phone = null;
+
+        try (BufferedReader br = new BufferedReader(new java.io.FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] keyValue = line.split(": ");
+                switch (keyValue[0]) {
+                    case "Name":
+                        name = keyValue[1];
+                        break;
+                    case "Age":
+                        age = Integer.parseInt(keyValue[1]);
+                        break;
+                    case "Email":
+                        email = keyValue[1];
+                        break;
+                    case "Phone":
+                        phone = Long.parseLong(keyValue[1]);
+                        break;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new Profile(name, age, email, phone);
     }
 }
